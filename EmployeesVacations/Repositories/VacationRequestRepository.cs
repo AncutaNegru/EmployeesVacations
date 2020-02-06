628eda32a0c3abf4c3c277592e9c7bf2c4e28036
ancuta.negru.morar@gmail.com <ancuta.negru.morar@gmail.com>
2020-02-06 19:20:38 +0200

    commit

--- EmployeesVacations/Repositories/VacationRequestRepository.cs
+++ EmployeesVacations/Repositories/VacationRequestRepository.cs
@@ -18,5 +18,23 @@ namespace EmployeesVacations.Repositories
         {
             this.dbContext = dbContext;
         }
+        private VacationRequestModel MapDbObjectToModel(VacationRequest dbVacationRequest)
+        {
+            VacationRequestModel vacationRequestModel = new VacationRequestModel();
+            if (dbVacationRequest != null)
+            {
+                vacationRequestModel.IDVacationRequest = dbVacationRequest.IDVacationRequest;
+                vacationRequestModel.IDEmployee = dbVacationRequest.IDEmployee;
+                vacationRequestModel.Reason = dbVacationRequest.Reason;
+                vacationRequestModel.StartDate = dbVacationRequest.StartDate;
+                vacationRequestModel.EndDate = dbVacationRequest.EndDate;
+                vacationRequestModel.DaysRequested = dbVacationRequest.DaysRequested;
+                vacationRequestModel.FirstApproval = (ApprovalStatusEnum)dbVacationRequest.FirstApproval;
+                vacationRequestModel.SecondApproval = (ApprovalStatusEnum)dbVacationRequest.SecondApproval;
+                //vacationRequestModel.Status = (ApprovalStatusEnum)dbVacationRequest.Status;
+                return vacationRequestModel;
+            }
+            return null;
+        }
     }
 }
