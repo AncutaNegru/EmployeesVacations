637341b5377a7890a2af04d3adc2af60b7aa7c01
ancuta.negru.morar@gmail.com <ancuta.negru.morar@gmail.com>
2020-02-08 12:45:10 +0200

    Added Vacation Request Controller.

---
+++ EmployeesVacations/Controllers/VacationRequestController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeesVacations.Models;
+using EmployeesVacations.Repositories;
+
+namespace EmployeesVacations.Controllers
+{
+    public class VacationRequestController : Controller
+    {
+        private VacationRequestRepository vacationRequestRepository = new VacationRequestRepository();
+        // GET: VacationRequest
+        public ActionResult Index()
+        {
+            List<VacationRequestModel> allVacationRequests = vacationRequestRepository.GetAllVacationRequests();
+            return View("Index", allVacationRequests);
+        }
+
+        // GET: VacationRequest/Details/5
+        public ActionResult Details(Guid id)
+        {
+            VacationRequestModel vacationRequestModel = vacationRequestRepository.GetVacationRequestById(id);
+            return View("DetailsVacationRequest", vacationRequestModel);
+        }
+
+        // GET: VacationRequest/Create
+        public ActionResult Create()
+        {
+            return View("CreateVacationRequest");
+        }
+
+        // POST: VacationRequest/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                VacationRequestModel vacationRequestModel = new VacationRequestModel();
+                UpdateModel(vacationRequestModel);
+                vacationRequestRepository.InsertVacationRequest(vacationRequestModel);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("CreateVacationRequest");
+            }
+        }
+
+        // GET: VacationRequest/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            VacationRequestModel vacationRequestModel = vacationRequestRepository.GetVacationRequestById(id);
+            return View("EditVacationRequest", vacationRequestModel);
+        }
+
+        // POST: VacationRequest/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Guid id, FormCollection collection)
+        {
+            try
+            {
+                VacationRequestModel vacationRequestModel = new VacationRequestModel();
+                UpdateModel(vacationRequestModel);
+                vacationRequestRepository.UpdateVacationRequest(vacationRequestModel);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("EditVacationRequest");
+            }
+        }
+
+        // GET: VacationRequest/Delete/5
+        public ActionResult Delete(Guid id)
+        {
+            VacationRequestModel vacationRequestModel = vacationRequestRepository.GetVacationRequestById(id);
+            return View("DeleteVacationRequest", vacationRequestModel);
+        }
+
+        // POST: VacationRequest/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Guid id, FormCollection collection)
+        {
+            try
+            {
+                vacationRequestRepository.DeleteVacationRequest(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("DeleteVacationrequest");
+            }
+        }
+    }
+}
