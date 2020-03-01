c43798b6cd85b8dc3fb8bd728fde4c393a891ce3
ancuta.negru.morar@gmail.com <ancuta.negru.morar@gmail.com>
2020-03-01 21:24:08 +0200

    Created EmployeVacationRequestViewModel.

---
+++ EmployeesVacations/ViewModels/EmployeeVacationRequestsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using EmployeesVacations.Models;
+using System;
+using System.Collections.Generic;
+
+namespace EmployeesVacations.ViewModels
+{
+    public class EmployeeVacationRequestsViewModel
+    {
+        public string FullName { get; set; }
+
+        public PositionEnum Position { get; set; }
+
+        public string TeamName { get; set; }
+
+        public string BusinessUnitName { get; set; }
+
+        public DateTime HiringDate { get; set; }
+
+        public int TotalDaysOff { get; set; }
+
+        public int DaysOffLeft { get; set; }
+
+        public List<VacationRequestModel> employeeVacations = new List<VacationRequestModel>();
+
+    }
+}
